2cdb14c1b484d5ab601b49d3513c3f99707c5e9a
agustinrobledo <agustinrobledo59.ar@gmail.com>
2021-11-13 15:45:48 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+const router = Router();
+
+router.get('/', (req, res, next) => {
+
+});
+
+router.get('/signup', (req, res, next) => {
+
+})
+
+router.post('/signup', (req, res, next) => {
+
+})
+
+router.get('/signin', (req, res, next) => {
+
+})
+
+router.post('/signin', (req, res, next) => {
+
+})
+export default router;
